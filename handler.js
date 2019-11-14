2e03cbd1dd4bdbbd99906eeec2d1d0b2146325a2
nec-trou <darya.bilyalova@gmail.com>
2019-11-14 21:04:37 +0500

    feat: add insertion from file to html

--- handler.js
+++ handler.js
@@ -1,19 +1,35 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function _handler(req, res) {
-  const path = `./dist/${req.url.substr(1)}`;
+  const path = req.url.substr(1);
+  if (path === 'favicon.ico') {
+    res.writeHead(206, { 'Content-Type': 'image/x-icon' });
+    res.end();
+  }
+  const targetPath = './template.html';
+  const target = '{read-file}';
+  const contentPath = `./dist/${path}`;
 
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      console.log(err);
-      res.end(`Sorry, it's nothing here. Please, try other path.`);
-      return;
-    }
+  fs.readFile(targetPath, (err, targetData) => {
+    let content = '';
+    const contentStream = fs.createReadStream(contentPath, 'utf8');
+
     res.writeHead(200, {
-      "Content-Type": "text/html; charset=utf-8"
+      'Content-Type': 'text/html; charset=utf-8'
     });
-    res.end(data);
+    contentStream
+      .on('data', function(chunk) {
+        content += chunk;
+      })
+      .on('end', function() {
+        targetData = targetData.toString().replace(target, content);
+        res.end(targetData);
+      })
+      .on('error', function(err) {
+        res.writeHead(500);
+        res.end(`Sorry, it's nothing here. Please, try other path.`);
+        console.log(`Not found ${err}`);
+      });
   });
 }
 
