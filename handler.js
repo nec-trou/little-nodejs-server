d67642384b6a22b974e0b49242d0a10b7465d280
nec-trou <darya.bilyalova@gmail.com>
2019-11-14 21:09:12 +0500

    feat: add writing to file on POST request

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const qs = require('querystring');
 
 function _handler(req, res) {
   const path = req.url.substr(1);
@@ -6,10 +7,28 @@ function _handler(req, res) {
     res.writeHead(206, { 'Content-Type': 'image/x-icon' });
     res.end();
   }
+
   const targetPath = './template.html';
   const target = '{read-file}';
   const contentPath = `./dist/${path}`;
 
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const writeStream = fs.createWriteStream(contentPath);
+      console.log(body);
+      body = qs.parse(body);
+      writeStream.write(body.content, 'utf8');
+      writeStream
+        .on('finish', () => {
+          console.log('Data is written to the file!');
+        })
+        .close();
+    });
+  }
   fs.readFile(targetPath, (err, targetData) => {
     let content = '';
     const contentStream = fs.createReadStream(contentPath, 'utf8');
@@ -18,14 +37,14 @@ function _handler(req, res) {
       'Content-Type': 'text/html; charset=utf-8'
     });
     contentStream
-      .on('data', function(chunk) {
+      .on('data', chunk => {
         content += chunk;
       })
-      .on('end', function() {
+      .on('end', () => {
         targetData = targetData.toString().replace(target, content);
         res.end(targetData);
       })
-      .on('error', function(err) {
+      .on('error', err => {
         res.writeHead(500);
         res.end(`Sorry, it's nothing here. Please, try other path.`);
         console.log(`Not found ${err}`);
